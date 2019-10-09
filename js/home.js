198595c2ccb7efb9f601d96c9e19a87eba8094f4
adelinel <adeline.latruwe@gmail.com>
2019-10-08 21:22:17 -0700

    section2 text and animation

--- js/home.js
+++ js/home.js
@@ -58,6 +58,19 @@ $(function() {
   .setClassToggle("#slidein3", "visible") // add class to reveal
   //.addIndicators() // add indicators (requires plugin)
   .addTo(controller);
+
+  var revealElements = document.getElementsByClassName("digit");
+  for (var i=0; i<revealElements.length; i++) { // create a scene for each element
+    new ScrollMagic.Scene({
+              triggerElement: revealElements[i], // y value not modified, so we can use element as trigger as well
+              offset: 50,												 // start a little later
+              triggerHook: 0.9,
+            })
+            .setClassToggle(revealElements[i], "visible") // add class toggle
+            //.addIndicators({name: "digit " + (i+1) }) // add indicators (requires plugin)
+            .addTo(controller);
+  }
+
 });
 
 
